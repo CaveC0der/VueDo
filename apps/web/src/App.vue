e9033954032165b341f951117ff2f8aadc80ef49
CaveC0der <wasylkoretskyi@gmail.com>
2024-04-13 02:59:01 +0300

    view transition

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,3 +1,24 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="view" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
+
+<style scoped>
+.view-enter-active,
+.view-leave-active {
+  height: 100dvh;
+  overflow: hidden;
+  transition: 0.25s ease-in-out;
+}
+
+.view-enter-from {
+  transform: translateX(-110%);
+}
+
+.view-leave-to {
+  transform: translateX(110%);
+}
+</style>
